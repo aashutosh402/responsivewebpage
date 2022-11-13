2be4d646519c4a9901c4478f6e6f2f346707b9cc
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2022-11-13 19:58:05 +0530

    gsap animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,11 +22,23 @@ document.addEventListener('mousemove',move)
 function move(e){
     this.querySelectorAll('.move').forEach(layer=>{
         const speed = layer.getAttribute('data-speed')
-        const x = (window.innerWidth - e.pageX*speed)/45
+        const x = (window.innerWidth - e.pageX*speed)/90
         const y = (window.innerHeight - e.pageY*speed)/45
  
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
 }
 
+
+// ====== Animation =======
+//navbar animation
+gsap.from('.nav_logo',{opacity:0,duration:1,delay:2,y:10})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2, y: 30, stagger: 0.2,})
+
+//======= home animation =======
+
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:2, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1, y: 30})
 
